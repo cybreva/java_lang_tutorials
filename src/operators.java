1106657bb5fe358e0d283f3d1f4954950e7c0824
Tanmay <mtvkhanna@gmail.com>
2026-07-02 15:37:20 +0530

    learned : arithmetic operators

---
+++ src/operators.java
@@ -0,0 +1,12 @@
+public class operators {
+    public static void main (string[] args){
+        int a = 10;
+        int b = 3;
+    //arithmatic operators {+  -  *  /  %}
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
