eb13caaca37e37d579abc8cb37fe3afbdefd065d
Tanmay <mtvkhanna@gmail.com>
2026-07-02 15:43:45 +0530

    learned : relational and logical operators

--- src/operators.java
+++ src/operators.java
@@ -1,5 +1,5 @@
 public class operators {
-    public static void main (string[] args){
+    public static void main (String[] args){
         int a = 10;
         int b = 3;
     //arithmatic operators {+  -  *  /  %}
@@ -8,5 +8,17 @@ public class operators {
         System.out.println(a * b);
         System.out.println(a / b);
         System.out.println(a % b);
+
+    //relational operator {==  !=  >  <  >=  <=}
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a < b);
+
+    //Logical Operators {// && || !}
+
+        System.out.println(a > 5 && b > 15);
+        System.out.println(a > 15 || b > 15);
+        System.out.println(!(a > b));
+
     }
 }
