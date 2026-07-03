c542853e61be3929b10bc395c7e4c68459ae51cb
Tanmay <mtvkhanna@gmail.com>
2026-07-03 09:50:53 +0530

    learned : ifelse:

---
+++ src/ifelse.java
@@ -0,0 +1,17 @@
+public class ifelse {
+    public static void main( String[] args) {
+
+
+
+                int age = 20;
+
+                if (age >= 18) {
+                    System.out.println("Eligible for Driving License");
+                }
+
+                System.out.println("Application Closed");
+
+
+        }
+    }
+
