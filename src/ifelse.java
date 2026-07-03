7147be728f4b0dcda36730c84611cb6693f27e6b
Tanmay <mtvkhanna@gmail.com>
2026-07-03 10:10:51 +0530

    learned :  else if and nested if else

--- src/ifelse.java
+++ src/ifelse.java
@@ -2,14 +2,24 @@ public class ifelse {
     public static void main( String[] args) {
 
 
+        int age = 100 ;
 
-                int age = 20;
 
-                if (age >= 18) {
-                    System.out.println("Eligible for Driving License");
-                }
-
-                System.out.println("Application Closed");
+        if (age >= 1100000) {
+            System.out.println("are you god");
+        }
+        else if (age >= 10912) {
+            System.out.println("you're literally older than the whole universe");
+        }
+        else if (age >= 100) {
+            System.out.println("are you really that old");
+        }
+        else if (age == 0) {
+            System.out.println("welcome to the world");
+        }
+        else {
+            System.out.println("are you yet to be born??");
+        }
 
 
         }
