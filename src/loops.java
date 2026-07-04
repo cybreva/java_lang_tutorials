8dbe629abe8244b839d4284964200c7ceefb3d50
Tanmay <mtvkhanna@gmail.com>
2026-07-04 13:14:35 +0530

    learned : loops

---
+++ src/loops.java
@@ -0,0 +1,24 @@
+public class loops {
+    public static void main(String[] args){
+
+        //for loop
+        for (int x = 1; x <= 5; x++) {
+            System.out.println(x);
+        }
+        //do-while loop
+        int i = 10;
+
+        do {
+            System.out.println(i);
+        } while (i < 5);
+        //break
+        for(int z=1;z<=10;z++){
+
+            if(z==5){
+                break;
+            }
+
+            System.out.println(z);
+        }
+    }
+}
