e3a84f0958e63759f2e323501df53a1ae62c8de2
Tanmay <mtvkhanna@gmail.com>
2026-07-19 15:58:35 +0530

    learned scanner

--- java_basics/UserInput.java
+++ java_basics/UserInput.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class UserInput {
+
     public static void main(String[] args) {
-        
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        System.out.print("Enter your height: ");
+        double height = sc.nextDouble();
+
+        System.out.println("\nUser Details");
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+
+        sc.close();
+
     }
 }
