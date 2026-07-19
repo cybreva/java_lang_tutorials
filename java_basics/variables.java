a551ed07a8b3e60a2a7178d5227897644e5d8a61
Tanmay <mtvkhanna@gmail.com>
2026-07-19 14:40:24 +0530

    retraqced files

---
+++ java_basics/variables.java
@@ -0,0 +1,12 @@
+public class variables {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+        int a = 1000 ;
+        int b = 1000 ;
+        System.out.println(a + b);
+        System.out.println("times");
+    }
+
+}
