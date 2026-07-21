2cd9a944eeac183c0129b9a88260d03333c8b910
Tanmay <mtvkhanna@gmail.com>
2026-07-21 12:18:43 +0530

    (OOP) : Learned : constructor overloading and method overloading

---
+++ oop/Lecture3.java
@@ -0,0 +1,36 @@
+
+class Student {
+    String name ;
+    String roll_number ;
+    String course ;
+
+    Student(){
+        name = "null" ;
+        roll_number = "null" ;
+        course = "how to be null" ;
+    }
+    Student(String name , String roll_number , String course){
+        this.name = name ;
+        this.roll_number = roll_number ;
+        this.course= course ;
+    }
+    Student(String name , String roll_number){
+        this.name = name ;
+        this.roll_number = roll_number ;
+    }
+    void display() {
+        System.out.println(name + " " + roll_number + " " + course);
+    }
+}
+public class Lecture3 {
+    public static void main(String[] args) {
+        Student s3 = new Student();
+        Student s1 = new Student("ravi" , "112341342" , "mathematical fuckkness");
+        Student s2 = new Student("rajma chaval" , "45643596754");
+
+        s1.display();
+        s2.display();
+        s3.display();
+    }
+    
+}
